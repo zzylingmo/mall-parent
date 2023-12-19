5bdbf933a0780d8e8dba3424807712e658c8d1df
zzy <qq905066802@gmail.com>
2023-12-19 17:21:24 +0800

    mall first

---
+++ mall-common/common-service/src/main/java/com.zzy.mall.common.config/Knife4jConfig.java
@@ -0,0 +1,40 @@
+package com.zzy.mall.common.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zzylm
+ * @description TODO
+ * @date 2023-12-19 15:44
+ */
+
+@Configuration
+public class Knife4jConfig {
+    @Bean
+    public GroupedOpenApi adminApi() {      // 创建了一个api接口的分组
+        return GroupedOpenApi.builder()
+                .group("admin-api")         // 分组名称
+                .pathsToMatch("/admin/**")  // 接口请求路径规则
+                .build();
+    }
+
+    /***
+     * @description 自定义接口信息
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("mall API接口文档")
+                        .version("1.0")
+                        .description("mall API接口文档")
+                        .contact(new Contact().name("atguigu"))); // 设定作者
+    }
+}
